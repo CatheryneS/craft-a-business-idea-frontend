32394df5b3461e50b76303a928c9bc74d03946fb
Catheryn Small <catheryn.small@gmail.com>
2021-02-26 01:55:59 -0500

    create starting page

---
+++ src/components/StartingPage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const StartingPage = () => {
+    return (
+        <div>
+            <h2>Instructions</h2>
+            <p>Welcome, you'll be coming up with ideas for three categories.</p>
+            <p>Once you have come up with ideas for each, we'll randomly pick one idea from each category,</p>
+            <p>which will be used to come up with a new business idea. Feel free to also watch the futur's video on Youtube</p>
+            <p>that this platform is based off of. You can always find a link to it in the nav bar.</p>
+
+            <h3>For each category use the prompts below to help you think of ideas.</h3>
+            <p>Don't worry you will access to these while filling out the forms.</p>
+            <h3>Love</h3>
+            <ul>
+                <li>What are you passionate about or what feels like "play"</li>
+                <li>What do in your spare time, when no one is watching?</li>
+                <li>If money weren't a concern, what would you do?</li>
+                <li>What would you pay to do?</li>
+                <li>What do you obsess over/dream about?</li>
+            </ul>
+            <h3>Good At</h3>
+            <ul>
+                <li>What are you paid to do?</li>
+                <li>What do you excel at naturally?</li>
+                <li>What did/will you study in school or been trained to do?</li>
+                <li>What do others often say you do easily? (Ask a friend if you need to.)</li>
+            </ul>
+            <h3>World Need</h3>
+            <ul>
+                <li>Think big(the world)</li>
+                <li>Think small(neighborhoods)</li>
+                <li>What opportunity to serve others?</li>
+                <li>What's trending right now?</li>
+            </ul>
+        </div>
+    )
+}
+
+export default StartingPage;
