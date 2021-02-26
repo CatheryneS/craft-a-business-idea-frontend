e18a23dde4b56e01a1e8acb9aefb78ab7dd8d1cc
Catheryn Small <catheryn.small@gmail.com>
2021-02-26 01:56:58 -0500

    added reset case for login form

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -1,7 +1,13 @@
-const currentUser = (state = { username: "", password: "" }, action) => {
+const initialState = {
+    username: "",
+    password: ""
+}
+const currentUser = (state = initialState, action) => {
     switch (action.type) {
         case "UPDATE_LOGIN_FORM":
             return action.credentials
+        case "REST_LOGIN_FORM":
+            return initialState
         default:
             return state;
     }
