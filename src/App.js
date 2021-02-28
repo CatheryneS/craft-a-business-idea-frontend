910f0cb75245673d07396d21d1f96f7554c4dea3
Catheryn Small <catheryn.small@gmail.com>
2021-02-27 23:29:03 -0500

    add worksheet route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NavBar from './containers/NavBar';
 import SignUp from './containers/SignUp';
 import LoginForm from './containers/LoginForm';
 import StartingPage from './components/StartingPage';
+import Worksheet from './containers/Worksheet';
 
 import { Route } from "react-router-dom";
 import Homepage from './containers/Homepage';
@@ -22,6 +23,7 @@ class App extends React.Component {
         <NavBar />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/worksheet" component={Worksheet} />
         <Route exact path="/" render={(() => this.props.currentUser ? <StartingPage /> : < Homepage />)} />
       </div>
     )
