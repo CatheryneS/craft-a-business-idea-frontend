7d71797ad7c6581acf490fa295ce1fc239fb115b
Catheryn Small <catheryn.small@gmail.com>
2021-03-02 11:48:48 -0500

    update current worksheet page to show details

--- src/components/CurrentWorksheet.js
+++ src/components/CurrentWorksheet.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchWorksheet } from '../actions/worksheet';
+import { LoveEntry, GoodAtEntry, WorldNeedEntry } from './Entry';
+import { Link } from 'react-router-dom';
 
 export class CurrentWorksheet extends Component {
     componentDidMount() {
         this.props.fetchWorksheet(this.props.id);
     }
-    render() {
-        const { worksheet } = this.props
 
+    loadLoves = () => {
+        const { worksheet } = this.props;
+        return worksheet.loves.map(love => < LoveEntry content={love} />)
+    }
+
+    loadGoodAt = () => {
+        const { worksheet } = this.props;
+        return worksheet.goodAts.map(g => < GoodAtEntry content={g} />)
+    }
+
+    loadWorldNeed = () => {
+        const { worksheet } = this.props;
+        return worksheet.worldNeeds.map(wn => < WorldNeedEntry content={wn} />)
+    }
+
+    render() {
         return (
             <div>
-                <h1>WELCOME TO THE CURRENT WORKSHEET PAGE!</h1>
-
+                <h1>Completed Worksheet</h1>
+                <h3>Love</h3>
+                <ul>
+                    {this.loadLoves()}
+                </ul>
+                <h3>Good At</h3>
+                <ul>
+                    {this.loadGoodAt()}
+                </ul>
+                <h3>World Need</h3>
+                <ul>
+                    {this.loadWorldNeed()}
+                </ul>
+                <button type="click"><Link to="/generator">Generate Ideas</Link></button>
             </div>
         )
     }
---
+++ src/components/Entry.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const LoveEntry = ({ content }) => {
+    return (
+        <>
+            <li>{content}</li>
+        </>
+    )
+}
+
+export const GoodAtEntry = ({ content }) => {
+    return (
+        <>
+            <li>{content}</li>
+        </>
+    )
+}
+
+export const WorldNeedEntry = ({ content }) => {
+    return (
+        <>
+            <li>{content}</li>
+        </>
+    )
+}
